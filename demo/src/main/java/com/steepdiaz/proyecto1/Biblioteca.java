de81e66634870e6f4af6dff365a2514276af588e
MichaelDiazV01 <mdiazvi4@estudiante.ibero.edu.co>
2024-11-30 17:05:17 -0500

    registro-libro

--- demo/src/main/java/com/steepdiaz/proyecto1/Biblioteca.java
+++ demo/src/main/java/com/steepdiaz/proyecto1/Biblioteca.java
@@ -9,10 +9,13 @@ public class Biblioteca {
         String eleccion = "";
         while (!eleccion.equals("9")) {
             System.out.println(
-                    "1. Registrar usuario\n7. Salir\nElige:");
+                    "1. Registrar usuario\n2. Registrar Libro\n7. Salir\nElige:");
             eleccion = sc.nextLine();
             if (eleccion.equals("1")) {
                 ControladorUsuarios.solicitarDatosParaRegistrar();
+            }
+            if (eleccion.equals("2")) {
+                ControladorLibros.solicitarDatosParaRegistrar();
             }
             if (eleccion.equals("7")) {
                 System.exit(0);
---
+++ demo/src/main/java/com/steepdiaz/proyecto1/Libro.java
@@ -0,0 +1,52 @@
+package com.steepdiaz.proyecto1;
+
+public class Libro {
+
+    private String codigo, titulo, autor;
+    private boolean disponible;
+
+    public Libro(String codigo, String titulo, String autor, boolean disponible) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = disponible;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "codigo=" + codigo + ", titulo=" + titulo + ", autor=" + autor +  ", disponible=" + disponible + '}';
+    }
+
+}
